248ab2b6e864e583c00a98c48572ab3d265205f7
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2023-03-10 22:25:59 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 $( "#element_2" ).click(function() { 
 
-if ($("#element_2").val() != '') { $("#element_1").val( $("#element_2").val() );  }
+//if ($("#element_2").val() != '') { $("#element_1").val( $("#element_2").val() );  }
+alert("Hola mundo");
 
 });
