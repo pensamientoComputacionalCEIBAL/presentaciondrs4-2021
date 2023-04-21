08551ff240d24cf76de2104ed4a34939a38485f8
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2023-04-21 10:19:43 -0300

    Update and rename v1.4.js to v1.5.js

--- v1.5.js
+++ v1.5.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
 
 $('#element_2').change(function(){ 
   switch($("#element_2 :selected").text()) {
-    case "Fernando Sabaño": $("#element_3").val('saba.fernando.daniel@gmail.com'); break;  
+    case "Fernando Sabaño": $("#element_3").val('saba.fernando.daniel@gmail.com'); break;
+    case "Bruno Oliva": $("#element_3").val('broliva@ceibal.edu.uy'); break;  
     case "Carolina Larronda": $("#element_3").val('clarronda@ceibal.edu.uy'); break;
     case "Carolina Romero": $("#element_3").val('cromero@ceibal.edu.uy'); break;
     case "Cinthia Núñez": $("#element_3").val('cinunez@ceibal.edu.uy'); break;
@@ -15,9 +16,9 @@ $('#element_2').change(function(){
     case "Federico Touya": $("#element_3").val('ftouya@ceibal.edu.uy'); break;
     case "Francisco Risso": $("#element_3").val('frisso@ceibal.edu.uy'); break;
     case "Irina Rodriguez": $("#element_3").val('irrodriguez@ceibal.edu.uy'); break;
+    case "Luciana Prudente": $("#element_3").val('lprudente@ceibal.edu.uy'); break;  
     case "Lourdes Vidal": $("#element_3").val('lvidal@ceibal.edu.uy'); break;
     case "Magela Araujo": $("#element_3").val('magaraujo@ceibal.edu.uy'); break;
-    case "Martin Mederos": $("#element_3").val('mamederos@ceibal.edu.uy'); break;
     case "Mónica González": $("#element_3").val('mogonzalez@ceibal.edu.uy'); break;
     case "Romina Etchepare": $("#element_3").val('roetchepare@ceibal.edu.uy'); break;
     case "Santiago Olivera": $("#element_3").val('solivera@ceibal.edu.uy'); break;
