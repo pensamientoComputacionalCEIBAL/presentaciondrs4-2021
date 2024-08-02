4c81ff59adcd5c957ef5cc91a80f49d2f29949ae
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2024-08-02 11:22:04 -0300

    Add files via upload

--- mf_cfe.js
+++ mf_cfe.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-let dir = "", refe = ""; $('#li_22').hide(); 
+let dir = "", refe = ""; $('#element_24').hide();
 
 function escribir () {
 if (dir != "") {
-  $(".anaPelea2").html('<p style="font-size:14px"><strong>Información de la sede:</strong></p>');
-  $(".anaPelea2").html( $(".anaPelea2").html() +  '<p style="font-size:14px">- Dirección: ' + dir + '</p>');
-  $(".anaPelea2").html( $(".anaPelea2").html() +  '<p style="font-size:14px">- Si tienes alguna duda, puedes contactarte con ' + refe + '</p>');
-  $('#li_22').show();  } else { $('#li_22').hide();  }
+  /* $(".anaPelea2").html('<p style="font-size:14px"><strong>Información de la sede:</strong></p>'); */
+  $("#element_24").html("");
+  $("#element_24").html( $("#element_24").html() +  '- Dirección: ' + dir + '\n\n');
+  $("#element_24").html( $("#element_24").html() +  '- Si tienes alguna duda, puedes contactarte con ' + refe);
+  $('#element_24').show(); } else { $('#element_24').hide(); }
 }
 
 function reiniciar () {
@@ -17,123 +18,111 @@ function reiniciar () {
 
 $('.anaPelea').on('propertychange input', function (e) {
 
+$("#element_19_1").prop('checked', false);
+$("#element_19_2").prop('checked', false);
+
 dir = "", refe = "";
 
 switch($(".anaPelea :selected").text()) {
   case "Artigas":
-    dir = 'IFD de Artigas, Gral. Fructuoso Rivera y Juan Antonio Lavalleja - Artigas';
-    refe = 'Silvana Salvador (<a href="ssalvador@ceibal.edu.uy">ssalvador@ceibal.edu.uy</a>)';
+    dir = 'IFD de Artigas, Gral. Fructuoso Rivera y Juan Antonio Lavalleja - departamento de Artigas';
+    refe = 'Silvana Salvador (ssalvador@ceibal.edu.uy)';
     break;
   case "Salto":
     dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Diego Texeira (<a href="dtexeira@ceibal.edu.uy">dtexeira@ceibal.edu.uy</a>)';
+    refe = 'Diego Texeira (dtexeira@ceibal.edu.uy)';
     break;
   case "Paysandú":
-    dir = 'Instituto de Formación Docente “Ercilia Guidali de Pisano”, Florida 1181 - Paysandú';
-    refe = 'Romina Etchepare (<a href="roetchepare@ceibal.edu.uy">roetchepare@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente “Ercilia Guidali de Pisano”, Florida 1181 - departamento de Paysandú';
+    refe = 'Romina Etchepare (roetchepare@ceibal.edu.uy)';
     break;
   case "Colonia":
-    dir = 'CERP del Suroeste, Domingo Maddalena 160 Colonia del Sacramento - Colonia';
-    refe = 'Francisco Risso (<a href="frisso@ceibal.edu.uy">frisso@ceibal.edu.uy</a>)';
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)';
+    break;
+  case "San José":
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)';
     break;
   case "Florida":
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Cristofer Cabrera (<a href="crcabrera@ceibal.edu.uy">crcabrera@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente de Florida, Pocho Fernandez 444 - departamento de Florida';
+    refe = 'Cristofer Cabrera (crcabrera@ceibal.edu.uy)';
     break;
   case "Montevideo":
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)';
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)';
     break;
   case "Maldonado":
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Tatiana Gilles (<a href="tgilles@ceibal.edu.uy">tgilles@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente de Maldonado, Rafael Perez del Puerto 651 - departamento de Maldonado';
+    refe = 'Tatiana Gilles (tgilles@ceibal.edu.uy)';
     break;
   case "Tacuarembó":
-    dir = 'Instituto de Formación Docente, Agraciada 892 - Rivera';
-    refe = 'Bruno Oliva Gonzalez (<a href="broliva@ceibal.edu.uy">broliva@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente, Agraciada 892 - departamento de Rivera';
+    refe = 'Bruno Oliva Gonzalez (broliva@ceibal.edu.uy)';
     break;
   case "Rivera":
-    dir = 'Instituto de Formación Docente, Agraciada 892 - Rivera';
-    refe = 'Bruno Oliva Gonzalez (<a href="broliva@ceibal.edu.uy">broliva@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente, Agraciada 892 - departamento de Rivera';
+    refe = 'Bruno Oliva Gonzalez (broliva@ceibal.edu.uy)';
     break;
   case "Cerro Largo":
-    dir = 'Instituto de Formación Docente, Manuel Lavalleja 1191 - Treinta y Tres';
-    refe = 'Carolina Romero (<a href="cromero@ceibal.edu.uy">cromero@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente, Manuel Lavalleja 1191 - departamento de Treinta y Tres';
+    refe = 'Carolina Romero (cromero@ceibal.edu.uy)';
     break;
   case "Treinta y Tres":
-    dir = 'Instituto de Formación Docente, Manuel Lavalleja 1191 - Treinta y Tres';
-    refe = 'Carolina Romero (<a href="cromero@ceibal.edu.uy">cromero@ceibal.edu.uy</a>)';
+    dir = 'Instituto de Formación Docente, Manuel Lavalleja 1191 - departamento de Treinta y Tres';
+    refe = 'Carolina Romero (cromero@ceibal.edu.uy)';
     break;
 } reiniciar (); escribir (); });
 
-$(function() {  $("#element_10").change( function() {
-  if ( $("#element_10 :selected").text()  == "Colonia (CERP Suroeste) - 7/9 10 horas" ) {
-    dir = 'CERP del Suroeste, Domingo Maddalena 160 Colonia del Sacramento - Colonia';
-    refe = 'Francisco Risso (<a href="frisso@ceibal.edu.uy">frisso@ceibal.edu.uy</a>)'; } 
-  else if ( $("#element_10 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
-  escribir ();
-}); });
-
-$(function() {  $("#element_11").change( function() {
-  if ( $("#element_11 :selected").text()  == "Colonia (CERP Suroeste) - 7/9 10 horas" ) {
-    dir = 'CERP del Suroeste, Domingo Maddalena 160 Colonia del Sacramento - Colonia';
-    refe = 'Francisco Risso (<a href="frisso@ceibal.edu.uy">frisso@ceibal.edu.uy</a>)'; } 
-  else if ( $("#element_11 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
-  escribir ();
-}); });
 
 $(function() {  $("#element_13").change( function() {
   if ( $("#element_13 :selected").text()  == "Florida (IFD Florida) - semana del 2/9 a definir" ) {
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Cristofer Cabrera (<a href="crcabrera@ceibal.edu.uy">crcabrera@ceibal.edu.uy</a>)'; } 
+    dir = 'Instituto de Formación Docente de Florida, Pocho Fernandez 444 - departamento de Florida';
+    refe = 'Cristofer Cabrera (crcabrera@ceibal.edu.uy)'; } 
   else if ( $("#element_13 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)'; }
   escribir ();
 }); });
 
 $(function() {  $("#element_16").change( function() {
   if ( $("#element_16 :selected").text()  == "Florida (IFD Florida) - semana del 2/9 a definir" ) {
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Cristofer Cabrera (<a href="crcabrera@ceibal.edu.uy">crcabrera@ceibal.edu.uy</a>)'; } 
+    dir = 'Instituto de Formación Docente de Florida, Pocho Fernandez 444 - departamento de Florida';
+    refe = 'Cristofer Cabrera (crcabrera@ceibal.edu.uy)'; } 
   else if ( $("#element_16 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)'; }
   escribir ();
 }); });
 
 
 $(function() {  $("#element_17").change( function() {
   if ( $("#element_17 :selected").text()  == "Canelones (IFD Pando) - 7/9 10 horas" ) {
-    dir = 'Instituto de Formación Docente de Pando, Transfiguración Iturria 1245 - Canelones';
-    refe = 'Facundo Costa (<a href="ecosta@ceibal.edu.uy">ecosta@ceibal.edu.uy</a>)'; } 
+    dir = 'Instituto de Formación Docente de Pando, Transfiguración Iturria 1245 - departamento de Canelones';
+    refe = 'Facundo Costa (ecosta@ceibal.edu.uy)'; } 
   else if ( $("#element_17 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)'; }
   escribir ();
 }); });
 
 $(function() {  $("#element_20").change( function() {
   if ( $("#element_20 :selected").text()  == "Maldonado (IFD Maldonado) - semana del 2/9 a definir" ) {
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Tatiana Gilles (<a href="tgilles@ceibal.edu.uy">tgilles@ceibal.edu.uy</a>)'; } 
+    dir = 'Instituto de Formación Docente de Maldonado, Rafael Perez del Puerto 651 - departamento de Maldonado';
+    refe = 'Tatiana Gilles (tgilles@ceibal.edu.uy)'; } 
   else if ( $("#element_20 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)'; }
   escribir ();
 }); });
 
 $(function() {  $("#element_21").change( function() {
   if ( $("#element_21 :selected").text()  == "Maldonado (IFD Maldonado) - semana del 2/9 a definir" ) {
-    dir = 'cuando la sede sea confirmada, te notificaremos vía email';
-    refe = 'Tatiana Gilles (<a href="tgilles@ceibal.edu.uy">tgilles@ceibal.edu.uy</a>)'; } 
+    dir = 'Instituto de Formación Docente de Maldonado, Rafael Perez del Puerto 651 - departamento de Maldonado';
+    refe = 'Tatiana Gilles (tgilles@ceibal.edu.uy)'; } 
   else if ( $("#element_21 :selected").text()  == "Montevideo (IINN) - 7/9 10 horas" ) {
-    dir = 'Institutos Normales de Montevideo, Soriano 1658 - Montevideo';
-    refe = 'Emilia Casaravilla (<a href="ecasaravilla@ceibal.edu.uy">ecasaravilla@ceibal.edu.uy</a>)'; }
+    dir = 'Institutos Normales de Montevideo, Soriano 1658 - departamento de Montevideo';
+    refe = 'Emilia Casaravilla (ecasaravilla@ceibal.edu.uy)'; }
   escribir ();
 }); });
 
