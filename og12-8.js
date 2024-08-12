0d2965c6303159c1d7b8212ff8e071a96ee9fe74
pensamientoComputacionalCEIBAL <71598555+pensamientoComputacionalCEIBAL@users.noreply.github.com>
2024-08-12 11:46:05 -0300

    Update and rename otras gestiones 2.js to og12-8.js

--- og12-8.js
+++ og12-8.js
@@ -2,16 +2,43 @@ console.log("Script funcionando correctamente :) 123");
 
 $(document).ready(function(){
 
+$('#element_2').change(function(){ 
+  switch($("#element_2 :selected").text()) {
+    case "Agustin Romano": $("#element_3").val('aromano@ceibal.edu.uy'); break;  
+    case "Bruno Oliva": $("#element_3").val('broliva@ceibal.edu.uy'); break;  
+    case "Carolina Larronda": $("#element_3").val('clarronda@ceibal.edu.uy'); break;
+    case "Carolina Romero": $("#element_3").val('cromero@ceibal.edu.uy'); break;
+    case "Cinthia Núñez": $("#element_3").val('cinunez@ceibal.edu.uy'); break;
+    case "Cristofer Cabrera": $("#element_3").val('crcabrera@ceibal.edu.uy'); break;
+    case "Diego Texeira": $("#element_3").val('dtexeira@ceibal.edu.uy'); break;
+    case "Denisse Fraga": $("#element_3").val('dfraga@ceibal.edu.uy'); break;
+    case "Emilia Casaravilla": $("#element_3").val('ecasaravilla@ceibal.edu.uy'); break;
+    case "Facundo Costa": $("#element_3").val('ecosta@ceibal.edu.uy'); break;
+    case "Federico Touya": $("#element_3").val('ftouya@ceibal.edu.uy'); break;
+    case "Francisco Risso": $("#element_3").val('frisso@ceibal.edu.uy'); break;
+    case "Irina Rodriguez": $("#element_3").val('irrodriguez@ceibal.edu.uy'); break;
+    case "Luciana Prudente": $("#element_3").val('lprudente@ceibal.edu.uy'); break;
+    case "Magela Araujo": $("#element_3").val('magaraujo@ceibal.edu.uy'); break;
+    case "Mónica González": $("#element_3").val('mogonzalez@ceibal.edu.uy'); break;
+    case "Natalia Salguero": $("#element_3").val('nsalguero@ceibal.edu.uy'); break;
+    case "Pablo Berocay": $("#element_3").val('pberocay@ceibal.edu.uy'); break;  
+    case "Renzo Cayrus": $("#element_3").val('rcayrus@ceibal.edu.uy'); break;  
+    case "Romina Etchepare": $("#element_3").val('roetchepare@ceibal.edu.uy'); break;
+    case "Santiago Olivera": $("#element_3").val('solivera@ceibal.edu.uy'); break;
+    case "Silvana Salvador": $("#element_3").val('ssalvador@ceibal.edu.uy'); break;
+    case "Tatiana Gilles": $("#element_3").val('tgilles@ceibal.edu.uy'); break;
+    case "Veda Contreras": $("#element_3").val('vcontreras@ceibal.edu.uy'); break;
+    case "Yenniffer de los Santos": $("#element_3").val('ydelossantos@ceibal.edu.uy'); break; }
+});
+
+
 // Al 'volver atrás', dejar vacíos los cuadros de textos.
 $("#submit_secondary" ).click(function() {
   $('input[type="text"]').val('');
   $('textarea').val('');
 } );
 
-
-
 $('.anaPelea').on('propertychange input', function (e) {
-
 
 if ($("#element_8_1").val() != '' && 
 $("#element_8_2").val() != '' && 
@@ -45,9 +72,9 @@ $("#element_10_2").val() != '') {
   let mensaje = "";
   
   if (dif_Minutos > 0) {
-  	
-  	if ( $("#element_37_1").is(":checked") == false) { cantidadDeCiclos24 = 0; }
-  	else { cantidadDeCiclos24 = 1; }
+    
+    if ( $("#element_37_1").is(":checked") == false) { cantidadDeCiclos24 = 0; }
+    else { cantidadDeCiclos24 = 1; }
   
     cantidadDias = Math.trunc(dif_Minutos  / 1440);
     
